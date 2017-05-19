b720ef78d48eb888a55879f0b3f3c2bc64ac7f70
Mark Caldropoli <mcaldro1@binghamton.edu>
2017-05-19 18:39:10 -0400

    Update UnitConverter.java

--- src/projects/UnitConverter.java
+++ src/projects/UnitConverter.java
@@ -3,10 +3,6 @@ package projects;
 import java.util.Scanner;
 
 public class UnitConverter {
-	/* Unit Converter (temp, currency, volume, mass and more) - Converts various units between one another. 
-	 * The user enters the type of unit being entered, the type of unit they want to convert to and then the value. 
-	 * The program will then make the conversion.
-	 */
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("What conversion would you like?");
